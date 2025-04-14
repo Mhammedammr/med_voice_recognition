37a988cc1f20d545e9e99c7687ca497c1125f295
mhammedammr <muhammedamr6@gmail.com>
2025-04-15 01:39:59 +0200

    optmizing project strcture

--- config.py
+++ config.py
@@ -7,19 +7,6 @@ load_dotenv()
 import os
 
 class Config:
-<<<<<<< HEAD
-    """Application configuration."""
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB max upload
-    ALLOWED_EXTENSIONS = {'mp3', 'wav', 'ogg'}
-    
-    DEBUG = True
-    TESTING = False
-    PORT = 8586
-    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "uploads")
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    FIREWORKS_API_KEY = os.getenv("FIREWORKS_API_KEY")
-    
-=======
     """Base configuration."""
 
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB max upload
@@ -27,15 +14,10 @@ class Config:
     DEBUG = True
     TESTING = False
     PORT = 8586
-    # LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "uploads")
-    # DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     FIREWORKS_API_KEY = os.getenv("FIREWORKS_API_KEY")
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
->>>>>>> 78d71f9 (optmizing project strcture)
     # Create upload folder if it doesn't exist
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
